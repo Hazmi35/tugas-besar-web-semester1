d1a286e626ee27829ed3898a86b56802e2efcdf6
Hazmi Alfarizqi <contact@hzmi.xyz>
2026-02-01 22:03:17 +0700

    feat: update business address from Jakarta Selatan to Bandung

--- src/lib/constants/business.js
+++ src/lib/constants/business.js
@@ -3,7 +3,7 @@ export const BUSINESS_INFO = {
   phone: "6281234567890", // Format wa.me (tanpa +62, gunakan 62 atau 0)
   phoneDisplay: "+62 812-3456-7890", // Format untuk display
   email: "hello@hepburnsbrownies.com",
-  address: "Jl. Raya Brownies No. 123, Jakarta Selatan",
+  address: "Jl. Raya Brownies No. 123, Bandung",
   operatingHours: "Senin - Minggu: 09.00 - 21.00 WIB",
   instagram: "https://www.instagram.com/hepburnsbrownie/",
 };
